1105fece1a3b9f2e29e5731b7e106561bfdb5311
ujashjogani <ujash@applaunch.io>
2025-03-23 23:05:33 +0530

    Switch to HashRouter for Netlify compatibility

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider><BrowserRouter><App /></BrowserRouter></ThemeProvider>
+    <ThemeProvider><HashRouter><App /></HashRouter></ThemeProvider>
   </React.StrictMode>
 );
 
